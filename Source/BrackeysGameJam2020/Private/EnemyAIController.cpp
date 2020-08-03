c72f76a0f758e3874a25fe11cc48a31344146e0d
Shaquille <shaquille-etienne@hotmail.com>
2020-08-03 23:07:39 +0100

    Added AI
    + AI finds the best cover around the player
    + Shoots a projectile towards target

---
+++ Source/BrackeysGameJam2020/Private/EnemyAIController.cpp
@@ -0,0 +1,41 @@
+
+
+#include "EnemyAIController.h"
+#include "EnemyBase.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyType_Enum.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+AEnemyAIController::AEnemyAIController()
+{
+	BehaviorComp = CreateDefaultSubobject<UBehaviorTreeComponent>(TEXT("BehaviorComp"));
+	BlackboardComp = CreateDefaultSubobject<UBlackboardComponent>(TEXT("BlackboardComp"));
+
+	TargetKeyName = "Target";
+}
+
+void AEnemyAIController::OnPossess(APawn* PossessPawn)
+{
+	Super::OnPossess(PossessPawn);
+
+	if (BehaviorComp == nullptr || BehaviorComp == nullptr)
+		return;
+
+	if (auto AICharacter = Cast<AEnemyBase>(PossessPawn))
+	{
+		if (AICharacter->BehaviorTree == nullptr || AICharacter->BehaviorTree->BlackboardAsset == nullptr)
+			return;
+
+		BlackboardComp->InitializeBlackboard(*(AICharacter->BehaviorTree->BlackboardAsset));
+		
+		BehaviorComp->StartTree(*AICharacter->BehaviorTree);
+	}
+}
+
+void AEnemyAIController::SetTarget(AActor* Target)
+{
+	BlackboardComp->SetValueAsObject(TargetKeyName, Target);
+}
+
+
